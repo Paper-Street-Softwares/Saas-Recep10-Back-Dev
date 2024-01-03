57b220a27c04264d2aeb0d1b8098a0cb6f2cf8c9
Edison Matos <edison.matos@live.com>
2024-01-02 23:02:42 -0300

    feat(structure): Create useCases

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,9 @@
+import UserUseCase from "../useCases/user-userCase";
+
+const userUserCase = new UserUseCase();
+
+class UserController {
+  async findAll(request: any, response: any) {}
+}
+
+export default UserController;
---
+++ src/useCases/user-userCase.ts
@@ -0,0 +1,5 @@
+import { request, response } from "express";
+
+class UserUseCase {}
+
+export default UserUseCase;
